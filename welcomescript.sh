8f7f9fcd2b971c097cce205b9f017dcc7d77a834
EC2 Default User <ec2-user@ip-172-31-26-59.us-east-2.compute.internal>
2024-06-02 00:54:04 +0000

    Backup current changes

--- welcomescript.sh
+++ welcomescript.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
-echo "Hello 312 Stadent!"
-echo "Inter Your Name Please:"
+<<<<<<< HEAD
+echo "Hello 312 Student!"
+echo "Enter Your Name Please:"
 read name
-echo "$names, welcome to 313 School"
+echo "$name, welcome to 312 School"
+=======
+echo "Hello 312 Student!"
+echo "Enter Your Name Please:"
+echo "new unnecessary changes" >> welcomescript.sh
+read name
+echo "$name, welcome to 312 School"
+>>>>>>> 53d755743d0f30b923287c23285fbd654b33d4c9
